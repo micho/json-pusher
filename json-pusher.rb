a60261950c5d8eeb78d6dc0c893ffca5e394bec6
Pablo Villalba <pablo@teambox.com>
2010-09-28 18:51:06 -0700

    Moving Redis connection to a method

--- json-pusher.rb
+++ json-pusher.rb
@@ -7,12 +7,14 @@ Pusher.app_id = 2192
 Pusher.key = 'c4d8da3b6c36091f0ac4'
 Pusher.secret = '50d3a48cd3e7133e7f55'
 
-begin
-  @@db = Redis.new
-rescue
-  puts "I couldn't connect to Redis, so I won't be storing the last messages for this channel.\nTo enable it, install Redis, run 'redis-server' and retry."
+def db
+  begin
+    @@db ||= Redis.new
+  rescue
+    msg = "I couldn't connect to Redis, so I won't be storing the last messages for this channel.\nTo enable it, install Redis, run 'redis-server' and retry."
+    @@display_once ||= puts(msg) || true
+  end
 end
-
 
 get '/' do
   "<p>This app echoes whatever you POST into /:channel to Pusher.</p>"
@@ -23,11 +25,11 @@ post '/' do
 end
 
 get '/:channel/last_messages.js' do
-  if @@db
+  if db
     <<-EOS
       document.on("dom:loaded", function(){
 
-        last_messages = (#{@@db["channels/#{params[:channel]}/last"] || "[]"});
+        last_messages = (#{db["channels/#{params[:channel]}/last"] || "[]"});
         last_messages.each(function(m) {
           console.log("Loading recent message " + m.id)
           delete m.id
@@ -56,18 +58,18 @@ def send_payload(data)
 end
 
 def save_to_redis(params)
-  if @@db
-    last_messages = JSON.parse(@@db["channels/#{params[:channel]}/last"] || "[]")
+  if db
+    last_messages = JSON.parse(db["channels/#{params[:channel]}/last"] || "[]")
     # Keep only the latest 20 messages
     last_messages.shift if last_messages.size == 19
     # Add the latest one
     last_messages.push(params)
     # Save to Redis
-    @@db["channels/#{params[:channel]}/last"] = JSON.dump(last_messages)
+    db["channels/#{params[:channel]}/last"] = JSON.dump(last_messages)
 
     # Save this key in Redis
     key = "channels/#{params[:channel]}/payloads/#{params[:id]}"
     puts "Saving to Redis: #{key}"
-    @@db[key] = JSON.dump(params)
+    db[key] = JSON.dump(params)
   end
 end
